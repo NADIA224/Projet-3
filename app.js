ac79ce416992564daa57439703affa87f6eea516
Nadia Bouskine <nad1236@hotmail.fr>
2021-02-17 19:23:53 +0100

    Modif

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const burger = document.querySelector('.burger');
-const menu = document.querySelector('nav');
+const menu = document.querySelector('nav.deuxiemenav');
+const liens = document.querySelectorAll('nav.deuxiemenav a');
 
 burger.addEventListener('click', function(){
     menu.classList.toggle('menu-open');
 });
 
+liens.forEach((lien) => {
+    lien.addEventListener('click', function () {
+        menu.classList.remove('nav-open');
+    } );
+}
+)
